2282e963748e222e42e2d84bab53e8e3844cb076
paultsimura <paultsimura@corp.doosan.com>
2020-05-06 17:55:52 +0300

    Reformat styles for cover adaptivity

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
 $(function() {
   $('.wrapper').addClass('loaded');
 
+  // Burger menu button handler
   $('.icon-menu').click(function() {
     $(this).toggleClass('active');
     $('.menu__body').toggleClass('active');
     $('.menu__logo').toggleClass('active');
     $('body').toggleClass('lock');
+  });
+
+  // Scrolling offset
+  const OFFSET = 60;
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
+      && location.hostname === this.hostname) {
+      let target = $(this.hash);
+      target = target.length ? target : $(`[name=${this.hash.slice(1)}]`);
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - (OFFSET - 2))
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
   });
 });
